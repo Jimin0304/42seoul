3b237a79bd2f421fe639a9390af3b4742eff6928
Jimin0304 <pjws0304@naver.com>
2024-01-05 13:45:50 +0900

    chore: OCCF private으로 이동

--- cpp_module/cpp_06/ex01/Serializer.hpp
+++ cpp_module/cpp_06/ex01/Serializer.hpp
@@ -12,16 +12,16 @@ class Serializer
 
 	public:
 
+		static uintptr_t serialize(Data* ptr);
+		static Data* deserialize(uintptr_t raw);
+
+	private:
+
 		Serializer();
 		Serializer( Serializer const & src );
 		~Serializer();
 
 		Serializer &		operator=( Serializer const & rhs );
-
-		static uintptr_t serialize(Data* ptr);
-		static Data* deserialize(uintptr_t raw);
-
-	private:
 
 };
 
