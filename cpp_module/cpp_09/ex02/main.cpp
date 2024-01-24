782b3d694c4a29e1b951b5de07ba516a45387c70
Jimin0304 <pjws0304@naver.com>
2024-01-24 15:06:45 +0900

    chore: sort 비교 테스트 추가

--- cpp_module/cpp_09/ex02/main.cpp
+++ cpp_module/cpp_09/ex02/main.cpp
@@ -1,5 +1,16 @@
 #include "PmergeMe.hpp"
 
+// // 비교 함수 정의
+// bool Compare(int a, int b) {
+//     return a < b;
+// }
+
+// // 정렬 함수
+// void SortNumbers(std::vector<int>& numbers) {
+//     // Compare 함수를 이용하여 정렬
+//     std::sort(numbers.begin(), numbers.end(), Compare);
+// }
+
 int main(int argc, char *argv[])
 {
     PmergeMe p;
@@ -7,8 +18,18 @@ int main(int argc, char *argv[])
     try {
         p.Sort(argc, argv);   
     } catch(const std::exception& e) {
-		  std::cerr << e.what() << std::endl;
+		std::cerr << e.what() << std::endl;
     }
+
+    // PmergeMe::PmergeVector vec;
+    // std::vector<int> numbers = vec.ParseArgv(argc, argv);
+    // SortNumbers(numbers);
+
+    // // 정렬 후 출력
+    // std::cout << "After: ";
+    // for (size_t i = 0; i < numbers.size(); ++i) {
+    //     std::cout << numbers[i] << " ";
+    // }
 
     return 0;
 }
