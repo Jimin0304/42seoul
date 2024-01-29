176b742f52538edbd7178a5bc5fd94b75d102e02
Jimin0304 <pjws0304@naver.com>
2024-01-29 21:29:03 +0900

    wordpress 추가 후 오류 수정 중

--- inception/srcs/requirements/mariadb/tools/start_mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/start_mariadb.sh
@@ -3,21 +3,17 @@
 # 명령어 실패하면 즉시 종료
 set -e
 
-MYSQL_SETUP_FILE=/var/lib/mysql/.setup
-
 service mariadb start
 
-if [ ! -e $MYSQL_SETUP_FILE ]; then # 처음에만 실행되도록 하기 위해서
+if [ ! -e /var/lib/mysql/$MYSQL_DATABASE ]; then # 처음에만 실행되도록 하기 위해서
 
 	# mysql -e : mysql 에서 명령을 실행하라는 의미
 	
 	mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE";
 	mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'";
-	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'";
-	mysql -e "FLUSH PRIVILEGES";
-	mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'";
+	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'; FLUSH PRIVILEGES;"
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
 
-	touch $MYSQL_SETUP_FILE
 fi
 
 mysql $MYSQL_DATABASE -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD
