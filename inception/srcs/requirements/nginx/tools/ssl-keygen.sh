e754f5cbc38ec6fdebf47dc12fa3688576d787d6
Jimin0304 <pjws0304@naver.com>
2024-02-05 18:18:58 +0900

    feat: inception 완료

--- inception/srcs/requirements/nginx/tools/ssl-keygen.sh
+++ inception/srcs/requirements/nginx/tools/ssl-keygen.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p /etc/nginx/ssl
 
 if [ ! -e /etc/nginx/ssl/jimpark.42.fr.crt ]; then
-    openssl req -newkey rsa:4096 -days 365 -nodes -x509 \
+    openssl req -newkey rsa:2048 -days 365 -nodes -x509 \
 		-subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Gam/CN=localhost" \
 		-out /etc/ssl/certs/jimpark.42.fr.crt \
 		-keyout /etc/ssl/private/jimpark.42.fr.key
