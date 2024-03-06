657a44294638c4885f392beb39d4c490cd45a75e
Jimin0304 <pjws0304@naver.com>
2024-03-06 16:05:54 +0900

    chore: VM에 맞게 파일 위치 조정

--- inception/srcs/requirements/nginx/tools/ssl-keygen.sh
+++ inception/srcs/requirements/nginx/tools/ssl-keygen.sh
@@ -6,7 +6,7 @@ mkdir -p /etc/nginx/ssl
 
 if [ ! -e /etc/nginx/ssl/jimpark.42.fr.crt ]; then
     openssl req -newkey rsa:2048 -days 365 -nodes -x509 \
-		-subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Gam/CN=localhost" \
+		-subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Gam/CN=jimpark.42.fr" \
 		-out /etc/ssl/certs/jimpark.42.fr.crt \
 		-keyout /etc/ssl/private/jimpark.42.fr.key
 fi
